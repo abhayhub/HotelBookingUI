e1a6ed34cfa68f1c3a93d1d001c9d2bc42ac88ae
Abhay Tiwari <abhayskp7948@gmail.com>
2024-03-03 02:25:17 +0530

    Implemented a Register form with validation and made a call to register endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 
 import {Routes, Route} from "react-router-dom"
 import Layout from "./layouts/Layout"
+import Register from "./pages/Register"
 
 
 function App() {
 
   return (
     <Routes>
-        <Route path="/" element={<Layout>helll ya baby</Layout>}/>
+        <Route path="/" element={<Layout>hii</Layout>}/>
+        <Route path="/register" element={<Layout><Register/></Layout>}/>
     </Routes>
   )
 }
